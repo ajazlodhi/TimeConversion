e1e81342122504fe3948175879f4797d5d2b9c68
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-02 23:35:19 -0500

    Create TimeConverter.java

---
+++ TimeConverter.java
@@ -0,0 +1,17 @@
+public class TimeConverter {
+    public static String timeConvert(int seconds){
+        int calculateminutes = seconds / 60;
+        int calculateHours = calculateminutes / 60;
+        int reminingMinutes = calculateminutes % 60;
+        int remainingSeconds = seconds % 60;
+        return calculateHours + "H " + reminingMinutes + "M " + remainingSeconds + " S";
+    }
+    public static String timeConcert(){
+        return timeConvert(3600);
+    }
+    public static void main(String[] args){
+        String result = timeConcert();
+        System.out.println(result);
+
+    }
+}
